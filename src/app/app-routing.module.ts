8341bf75c6ac6db0f0b5de39ea032688529cd032
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2021-10-04 17:22:25 -0400

    default home page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,12 @@ import {CommonModule} from "@angular/common";
 import {LoginPageComponent} from "./login-page/login-page.component";
 import {RegisterComponent} from "./register/register.component";
 import {AppComponent} from "./app.component";
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent},
-  { path: 'register', component: RegisterComponent}
+  { path: 'register', component: RegisterComponent},
+  { path: '', component: HomeComponent}
 ];
 
 @NgModule({
