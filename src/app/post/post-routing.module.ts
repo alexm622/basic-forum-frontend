5c20684cfc3a0dd2c4db0e6a8f29600af55f5382
Alex Comeau <17982529+alexm622@users.noreply.github.com>
2021-10-10 17:10:33 -0400

    fixed routing

--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -9,10 +9,10 @@ import {PostsComponent} from "./posts/posts.component";
 
 const routes: Routes = [
   { path: 'c/:page', component: CategoriesComponent},
-  { path: 'c/:cat_id/:page', component:PostsComponent},
-  { path: 'p/:cat_id/:post_id', component: PostComponent},
   { path: 'c/:page/new', loadChildren: () => import(/*webpackChunkName: "create" */'./create-item/create-item.module').then(m => m.CreateItemModule)},
+  { path: 'c/:cat_id/:page', component:PostsComponent},
   { path: 'c/:cat_id/:page/new', loadChildren: () => import(/*webpackChunkName: "create" */'./create-item/create-item.module').then(m => m.CreateItemModule)},
+  { path: 'p/:cat_id/:post_id', component: PostComponent},
   { path: 'p/:cat_id/:post_id/new', loadChildren: () => import(/*webpackChunkName: "create" */'./create-item/create-item.module').then(m => m.CreateItemModule)},
 ];
 
